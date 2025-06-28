9b04f636bc6851df22abd975260ea33041a885db
Your Name <your.email@example.com>
2025-06-28 15:47:37 +0100

    api integration

--- src/components/Admin/Dashboard/Transaction/MobileTransaction.jsx
+++ src/components/Admin/Dashboard/Transaction/MobileTransaction.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import styles from "./style.module.css";
 import time from "../../../../assets/Dashboard/time.svg";
-import right from "../../../../assets/Dashboard/icon.svg";
+import right from "../../../../assets/Dashboard/Icon.svg";
 import csv from "../../../../assets/Dashboard/csv.svg";
 import vector from "../../../../assets/Dashboard/Vector.svg";
 import copy from "../../../../assets/Dashboard/copy.svg";
